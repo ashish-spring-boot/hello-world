571d7cb85b1237c09eea5252107da90720bb5852
Ashish Adhikari <ashish.adhikari@infosys.com>
2018-03-12 07:14:07 +0000

    print hello world on the console

--- src/main/java/boot/spring/helloworld/HelloWorldApplication.java
+++ src/main/java/boot/spring/helloworld/HelloWorldApplication.java
@@ -1,12 +1,22 @@
 package boot.spring.helloworld;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelloWorldApplication {
+public class HelloWorldApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.springframework.boot.CommandLineRunner#run(java.lang.String[])
+	 */
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello World!");
+		
 	}
 }
